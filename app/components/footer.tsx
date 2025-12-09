f812cfbb53b561d076027cae742c70b692ad92d8
Rohit <rohitc129@gmail.com>
2025-12-09 22:18:02 +0530

    basepath

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -12,7 +12,7 @@ export default function Footer({
         <footer className="container max-w-full" ref={contactUsRef}>
             <div className="flex flex-col md:flex-row gap-10 md:gap-0 lg:gap-10 xl:gap-5 px-5 lg:px-10 xl:px-25 py-10 bg-[#f6f6f6] text-black">
                 <div className="w-full md:w-[40%] xl:w-[30%] flex flex-col gap-5 md:pr-15 xl:pr-10 text-center items-center md:items-start md:text-left">
-                    <Image src="/images/icons/century-50-years.svg" alt="Century 50 Years" width={300} height={100} />
+                    <Image src="images/icons/century-50-years.svg" alt="Century 50 Years" width={300} height={100} />
                     <p className="text-lg xl:text-[20px] font-corporate-light leading-[1.2]">Suruchi Properties Private Limited  is a Real Estate Company primarily headquartered in Bangalore and is a Wholly-owned Subsidiary Company of Century Joint Developments Private Limited.</p>
                 </div>
                 <div className="w-full md:w-[60%] xl:w-[70%] flex flex-col gap-10 font-corporate-regular font-medium">
@@ -32,11 +32,11 @@ export default function Footer({
                     </div>
                     <div className="flex flex-col xl:flex-row gap-5 md:gap-2 lg:gap-5 items-center md:items-left md:justify-between text-xl md:text-lg lg:text-xl">
                         <div className="flex gap-2">
-                            <Image src="/images/icons/call-icon.svg" alt="Call Icon" width={25} height={25} />
+                            <Image src="images/icons/call-icon.svg" alt="Call Icon" width={25} height={25} />
                             <Link href="tel:+918040453453">080-40453453</Link>
                         </div>
                         <div className="flex gap-2">
-                            <Image src="/images/icons/mail-icon.svg" alt="Mail Icon" width={25} height={25} />
+                            <Image src="images/icons/mail-icon.svg" alt="Mail Icon" width={25} height={25} />
                             <Link href="mailto:suruchi@centuryrealestate.in">suruchi@centuryrealestate.in</Link>
                         </div>
                         <div>
@@ -48,16 +48,16 @@ export default function Footer({
             <div className="bg-[#9f1634] flex flex-col justify-center items-center md:items-end h-15 px-20">
                 <ul className="flex gap-10">
                     <li>
-                        <Link href="https://www.facebook.com/centuryrealestatebangalore/" target="_blank"><Image src="/images/icons/facebook.svg" alt="Facebook" width={20} height={20} /></Link>
+                        <Link href="https://www.facebook.com/centuryrealestatebangalore/" target="_blank"><Image src="images/icons/facebook.svg" alt="Facebook" width={20} height={20} /></Link>
                     </li>
                     <li>
-                        <Link href="https://www.instagram.com/centuryrealestate/" target="_blank"><Image src="/images/icons/instagra.svg" alt="Instagram" width={20} height={20} /></Link>
+                        <Link href="https://www.instagram.com/centuryrealestate/" target="_blank"><Image src="images/icons/instagra.svg" alt="Instagram" width={20} height={20} /></Link>
                     </li>
                     <li>
-                        <Link href="https://x.com/centuryblr" target="_blank"><Image src="/images/icons/x.svg" alt="X" width={20} height={20} /></Link>
+                        <Link href="https://x.com/centuryblr" target="_blank"><Image src="images/icons/x.svg" alt="X" width={20} height={20} /></Link>
                     </li>
                     <li>
-                        <Link href="https://www.linkedin.com/company/century-real-estate-holdings-pvt-ltd" target="_blank"><Image src="/images/icons/linkedin.svg" alt="Linked IN" width={20} height={20} /></Link>
+                        <Link href="https://www.linkedin.com/company/century-real-estate-holdings-pvt-ltd" target="_blank"><Image src="images/icons/linkedin.svg" alt="Linked IN" width={20} height={20} /></Link>
                     </li>
                 </ul>
             </div>
