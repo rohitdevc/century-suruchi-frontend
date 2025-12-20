4309b7b1c08583666fefddbfb2f07f7f267f9cf2
Rohit <rohitc129@gmail.com>
2025-12-20 21:42:54 +0530

    updated remote url

--- next.config.ts
+++ next.config.ts
@@ -16,7 +16,7 @@ const nextConfig: NextConfig = {
       },
       {
         protocol: 'https',
-        hostname: 'www.centuryrealestate.in',
+        hostname: 'suruchi.centuryrealestate.in',
         port: '',
         pathname: '/**'
       },
