946e3df89567e3cd66d65c41e47fc3ffe0aa25ae
Rohit <rohitc129@gmail.com>
2025-12-29 11:02:29 +0530

    installed ga

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,6 @@
 import "./globals.css";
+import Script from "next/script";
+
 
 export default function RootLayout({
   children,
@@ -7,6 +9,21 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        {/* Google Analytics */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-10NWF7PYV4"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-10NWF7PYV4');
+          `}
+        </Script>
+      </head>
       <body>
         {children}
       </body>
